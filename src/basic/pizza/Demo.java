4d6005b527f6df119eea0c666994da7487bc2060
mhshahib <mh.shahib@gmail.com>
2020-07-11 16:36:46 -0400

    basic java class done and move on intelliJ from eclipse

---
+++ src/basic/pizza/Demo.java
@@ -0,0 +1,5 @@
+package basic.pizza;
+
+public class Demo {
+
+}
---
+++ src/pizza/Pizza.java
@@ -0,0 +1,17 @@
+package pizza;
+
+import basic.LearnUseAccessModifier;
+
+public class Pizza {
+
+	
+	
+	public static void main(String[] args) {
+		
+		LearnUseAccessModifier.TvDisplay();
+		
+	}
+	
+	
+	
+}
